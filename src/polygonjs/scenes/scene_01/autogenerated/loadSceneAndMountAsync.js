b1f116c013dcd21cf7bd88f932c406442f884b72
Guillaume Fradin <guillaumefradin@gmail.com>
2022-03-28 20:47:18 +0100

    upgrade polygonjs and plugin to 1.1.246

--- src/polygonjs/scenes/scene_01/autogenerated/loadSceneAndMountAsync.js
+++ src/polygonjs/scenes/scene_01/autogenerated/loadSceneAndMountAsync.js
@@ -1,13 +1,21 @@
-import {loadSceneData_scene_01} from './loadSceneData.js?t=1643125142188';
+import {loadSceneData_scene_01} from './loadSceneData.js?t=1648496786774';
 
 export const loadSceneAndMountAsync_scene_01 = async function (options) {
-	const {onProgress, domElement, configureSceneData} = options;
+	const {
+		onProgress,
+		domElement,
+		configureSceneData,
+		autoPlay,
+		sceneDataRoot,
+		assetsRoot,
+		libsRootPrefix
+	} = options;
 	const runRegister = options.runRegister != null ? options.runRegister : true;
 	const loadModules = options.loadModules != null ? options.loadModules : true;
 
 	const moduleNames = loadModules ? ["EXRLoader"] : [];
 	const modulePromises = moduleNames.map((moduleName) => import('./modules/' + moduleName + (options.moduleImportSuffix || '')));
-	const promises = [import('./loadSceneFromSceneData.js?t=1643125142188'), loadSceneData_scene_01({onProgress}), ...modulePromises];
+	const promises = [import('./loadSceneFromSceneData.js?t=1648496786774'), loadSceneData_scene_01({onProgress, sceneDataRoot}), ...modulePromises];
 	const results = await Promise.all(promises);
 	const {Poly, loadSceneFromSceneData_scene_01} = results[0];
 	const sceneData = results[1];
@@ -27,6 +35,14 @@ export const loadSceneAndMountAsync_scene_01 = async function (options) {
 		i++;
 	}
 
-	const loadedData = await loadSceneFromSceneData_scene_01({onProgress, sceneData, domElement, runRegister});
+	const loadedData = await loadSceneFromSceneData_scene_01({
+		onProgress,
+		sceneData,
+		domElement,
+		runRegister,
+		autoPlay,
+		assetsRoot,
+		libsRootPrefix
+	});
 	return loadedData;
 };
--- src/polygonjs/scenes/scene_01/autogenerated/loadSceneFromSceneData.d.ts
+++ src/polygonjs/scenes/scene_01/autogenerated/loadSceneFromSceneData.d.ts
@@ -1,26 +1,24 @@
-import { ScenePlayerImporter } from '@polygonjs/polygonjs/dist/src/engine/io/player/Scene';
-import {configureScene} from '../PolyConfig';
 import {Poly} from '@polygonjs/polygonjs/dist/src/engine/Poly';
-import {AllExpressionsRegister} from '@polygonjs/polygonjs/dist/src/engine/poly/registers/expressions/All';
-import {configurePolygonjs} from '../PolyConfig';
-import {requiredImports_scene_01} from './requiredImports';
-import { SceneJsonExporterData } from '@polygonjs/polygonjs/dist/src/engine/io/json/export/Scene';
-import { OnProgressCallback } from './loadSceneData';
-import { PolySceneWithNodeMap_scene_01 } from './PolySceneWithNodeMap';
-import { BaseViewerType } from '@polygonjs/polygonjs/dist/src/engine/viewers/_Base';
+import {SceneJsonExporterData} from '@polygonjs/polygonjs/dist/src/engine/io/json/export/Scene';
+import {OnProgressCallback} from './loadSceneData';
+import {PolySceneWithNodeMap_scene_01} from './PolySceneWithNodeMap';
+import {BaseViewerType} from '@polygonjs/polygonjs/dist/src/engine/viewers/_Base';
 
 export interface loadSceneOptions {
 	sceneData: SceneJsonExporterData;
 	onProgress: OnProgressCallback;
+	autoPlay?: boolean;
 	domElement?: HTMLElement;
 	runRegister?: boolean;
+	assetsRoot?: string;
+	libsRootPrefix?: string;
 }
 interface LoadedData {
-	scene: PolySceneWithNodeMap_scene_01,
-	viewer: BaseViewerType|undefined
+	scene: PolySceneWithNodeMap_scene_01;
+	viewer: BaseViewerType | undefined;
 }
-export type LoadSceneMethod = (options: loadSceneOptions) => Promise<LoadedData|undefined>;
+export type LoadSceneMethod = (options: loadSceneOptions) => Promise<LoadedData | undefined>;
 
-const loadSceneFromSceneData_scene_01: LoadSceneMethod;
+declare const loadSceneFromSceneData_scene_01: LoadSceneMethod;
 
 export {Poly, loadSceneFromSceneData_scene_01};
